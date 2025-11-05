9b188e3f04f9be2002f8370500b30698a9bb24ce
Mike <2023postline@proton.me>
2025-11-05 12:31:03 +0100

    Configured github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: 'https://mgrushko.dev',
+  base: '/',
   integrations: [vue()],
 
   vite: {
